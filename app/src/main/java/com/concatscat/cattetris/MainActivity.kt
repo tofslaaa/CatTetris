f96528f024b3d2940b371d7c67733b822246b6b8
Anastasiya Remontova <a.remontova@makeapp.ru>
2019-12-04 16:51:53 +0300

    Clear project

--- app/src/main/java/com/concatscat/cattetris/MainActivity.kt
+++ app/src/main/java/com/concatscat/cattetris/MainActivity.kt
@@ -7,61 +7,10 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var tetris: TetrisView? = null
-    var timer = Timer()
-    var gameStarted = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainContentLayout.post {
-            mainContentLayout.setOnClickListener {
-                if (gameStarted) {
-                    stopGameLoop()
-                    removeGameView()
-                } else {
-                    addGameView()
-                    startGameLoop()
-                }
-            }
-        }
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        stopGameLoop()
-    }
-
-    private fun addGameView() {
-        if (tetris == null) {
-            tetris = TetrisView(this)
-            mainContentLayout.addView(tetris)
-        }
-    }
-
-    fun removeGameView() {
-        mainContentLayout.removeView(tetris)
-        tetris = null
-    }
-
-    fun startGameLoop() {
-        timer = Timer()
-
-        val gameTimerTask = object : TimerTask() {
-            override fun run() {
-                runOnUiThread {
-                    tetris?.loop()
-                }
-            }
-        }
-
-        timer.schedule(gameTimerTask, 0, 20)
-        gameStarted = true
-    }
-
-    fun stopGameLoop() {
-        timer.cancel()
-        gameStarted = false
     }
 }
