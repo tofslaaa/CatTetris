69db31e5a9a49b2ddd54934d9b29681534baa79a
Anastasiya Remontova <a.remontova@makeapp.ru>
2019-12-05 19:39:37 +0300

    Created presenter

---
+++ app/src/main/java/com/concatscat/cattetris/Presenter.kt
@@ -0,0 +1,30 @@
+package com.concatscat.cattetris
+
+import android.graphics.drawable.Drawable
+
+class Presenter(private val view: View) {
+
+    fun generateBlocks() {
+        val positions: List<Int> = mutableListOf(
+            0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+        )
+
+        view.setAdapter(positions)
+    }
+
+
+    interface View {
+        fun setAdapter(positions: List<Int>)
+    }
+}
