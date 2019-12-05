64147d1cc51c6ed701c53964fc61948d6de8e11b
Anastasiya Remontova <a.remontova@makeapp.ru>
2019-12-05 19:39:51 +0300

    Implemented recycler view logic

--- app/src/main/java/com/concatscat/cattetris/MainActivity.kt
+++ app/src/main/java/com/concatscat/cattetris/MainActivity.kt
@@ -2,15 +2,40 @@ package com.concatscat.cattetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mBlocksAdapter: BlocksAdapter
+    lateinit var presenter: Presenter
+
+    private var mPositions: List<Int> = (0..119).map { 0 }.toList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        presenter = Presenter(view)
 
+        mBlocksAdapter = BlocksAdapter(this, mPositions)
+
+        play_field.layoutManager = GridLayoutManager(this, 10)
+        play_field.adapter = mBlocksAdapter
+
+
+        play_button.setOnClickListener {
+            presenter.generateBlocks()
+        }
+    }
+
+    private val view: Presenter.View = object : Presenter.View{
+        override fun setAdapter(positions: List<Int>) {
+            cat.visibility = View.INVISIBLE
+            start_text.visibility = View.INVISIBLE
+
+            mBlocksAdapter.setNewList(positions)
+        }
     }
 }
