9a98aa87f367dce5150802e5aab6761bc0272380
Anastasiya Remontova <a.remontova@makeapp.ru>
2019-12-10 13:38:19 +0300

    Created ticker logic class

---
+++ app/src/main/java/com/concatscat/cattetris/logic/Ticker.kt
@@ -0,0 +1,33 @@
+package com.concatscat.cattetris.logic
+
+import android.os.Handler
+
+class Ticker(private val interval: Long) {
+
+    val handler = Handler()
+    private var onTick: (() -> Unit)? = null
+
+    var running = false
+
+    private val runnable: Runnable by lazy {
+        Runnable {
+            if (running) {
+                onTick?.invoke()
+                handler.postDelayed(runnable, interval)
+            }
+        }
+    }
+
+    fun start(onTick: () -> Unit) {
+        handler.removeCallbacks(runnable)
+        this.onTick = onTick
+        running = true
+        handler.postDelayed(runnable, interval)
+    }
+
+    fun stop() {
+        handler.removeCallbacks(runnable)
+        onTick = null
+        running = false
+    }
+}
