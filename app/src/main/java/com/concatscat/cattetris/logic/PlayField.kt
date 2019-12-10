38bc1c4b0819f7167d90a2069653302951e0d702
Anastasiya Remontova <a.remontova@makeapp.ru>
2019-12-10 13:38:09 +0300

    Created play field logic class

---
+++ app/src/main/java/com/concatscat/cattetris/logic/PlayField.kt
@@ -0,0 +1,118 @@
+package com.concatscat.cattetris.logic
+
+import android.util.Log
+import com.concatscat.cattetris.BlockModel
+
+class PlayField(val width: Int, val height: Int = width, private val listener: Listener) {
+
+    val TAG = PlayField::class.java.simpleName
+
+    val size = width * height
+
+    var figure: Figure = Figure.None
+
+    var matrix = Matrix(width, height)
+
+    private var top = height
+
+    fun updateTick() {
+        val collides = collides()
+        if (collides) {
+            mergeFigure()
+            update()
+            listener.onCollides()
+        } else {
+            figure.coordinates.moveDown()
+            update()
+        }
+
+        Log.d(TAG, merged().toString())
+    }
+
+    fun update() {
+        listener.onUpdated(merged())
+    }
+
+    fun merged() =
+        matrix.clone().apply {
+            for ((x, y) in figure.blocksIterator()) {
+                this[x, y] = mapFigureType()
+            }
+        }
+
+    private fun mergeFigure() {
+        for ((x, y) in figure.blocksIterator()) {
+            matrix[x, y] = mapFigureType()
+        }
+        if (figure.top < top) {
+            top = figure.top
+        }
+    }
+
+    private fun mapFigureType(): Int {
+        return when (figure) {
+            is Figure.None -> BlockModel.NONE.type
+            is Figure.Line -> BlockModel.LINE.type
+            is Figure.GunLeft -> BlockModel.GUN.type
+            is Figure.GunRight -> BlockModel.GUN.type
+            is Figure.SnakeLeft -> BlockModel.SNAKE.type
+            is Figure.SnakeRight -> BlockModel.SNAKE.type
+            is Figure.Square -> BlockModel.SQUARE.type
+            is Figure.Stairs -> BlockModel.STAIRS.type
+        }
+    }
+
+    fun removeFilledLines() {
+        for (row in matrix.array) {
+            if (!row.contains(0)) {
+                for (j in row.indices) {
+                    row[j] = 0
+                }
+            }
+        }
+    }
+
+    fun fall() {
+        val emptyRows = mutableListOf<Int>()
+        for (rowIndex in matrix.array.indices.reversed()) {
+            if (rowIndex < top) {
+                continue
+            }
+            val row = matrix.array[rowIndex]
+            val isEmptyRow = row.all { it == 0 }
+            if (isEmptyRow) {
+                emptyRows.add(rowIndex)
+            }
+        }
+
+        for (emptyRowIndex in emptyRows.reversed()) {
+            for (rowIndex in emptyRowIndex - 1 downTo 0) {
+                val row = matrix.array[emptyRowIndex]
+                for (columnIndex in row.indices) {
+                    matrix[columnIndex, rowIndex + 1] = matrix[columnIndex, rowIndex]
+                }
+            }
+        }
+    }
+
+    private fun collides() =
+        figure.blocksIterator()
+            .asSequence()
+            .filter { (x, y) ->
+                figure.bottom >= height || matrix[x, y + 1] != 0
+            }
+            .any()
+
+    fun clear() {
+        figure = Figure.None
+        matrix = Matrix(width, height)
+        top = height
+    }
+
+    interface Listener {
+
+        fun onCollides()
+        fun onUpdated(matrix: Matrix)
+    }
+
+}
