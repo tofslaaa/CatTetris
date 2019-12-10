782b0ebcdea2ab723542e5ab264788b08702559c
Anastasiya Remontova <a.remontova@makeapp.ru>
2019-12-10 14:04:27 +0300

    Reformatted block model enum to data

--- app/src/main/java/com/concatscat/cattetris/BlockModel.kt
+++ app/src/main/java/com/concatscat/cattetris/BlockModel.kt
@@ -1,10 +1,13 @@
 package com.concatscat.cattetris
 
-enum class BlockModel (val type: Int){
-    NONE(0),
-    GUN(1),
-    LINE(2),
-    SQUARE(3),
-    SNAKE(4),
-    STAIRS(5)
+data class BlockModel (val type: Type = Type.NONE) {
+
+    enum class Type(val type: Int) {
+        NONE(0),
+        GUN(1),
+        LINE(2),
+        SQUARE(3),
+        SNAKE(4),
+        STAIRS(5),
+    }
 }
