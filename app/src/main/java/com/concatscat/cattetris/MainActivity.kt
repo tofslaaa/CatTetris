773c6594e29651ce30432460acf820fc3d8ec285
Anastasiya Remontova <a.remontova@makeapp.ru>
2019-12-11 11:31:27 +0300

    Refactored code

--- app/src/main/java/com/concatscat/cattetris/MainActivity.kt
+++ app/src/main/java/com/concatscat/cattetris/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
